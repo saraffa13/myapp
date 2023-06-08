7e31f31a1be0a679522d1bd4c6f453285230a461
Shivam <ssaraffa786@gmail.com>
2023-06-08 19:13:44 +0530

    Random color generator

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,6 +8,13 @@ const Button = () => {
     const[count,setCount] = useState(1);
     const[colorBool,setColorBool] = useState(false);
 
+    const [randomColor, setRandomColor] = useState('#000000');
+
+    const generateRandomColor = () => {
+        const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        setRandomColor(color);
+    };
+
     const counterIncrementer = () => {
         setCount(count=>count+10);
     }
@@ -19,12 +26,14 @@ const Button = () => {
     return (
         <div className={classes.Button}>
             <div className={classes.circleHolder}>
-                <div className={classes.circle}>
+                <div className={classes.circle} onClick={counterIncrementer}>
                     <Cendol count={count}/>
-                    <button className={classes.counterIncrementer} onClick={counterIncrementer}>Click to increase counter</button>
+                    <button className={classes.counterIncrementer}>Click to increase counter</button>
                 </div>
             </div>
-            <button className={`${classes.colorChanger} ${colorBool?classes.col2:classes.col1}`} onClick={colorChanger}>Change Color</button>
+            <button style={{
+                backgroundColor: randomColor
+            }} className={classes.colorChanger} onClick={generateRandomColor}>Change Color</button>
         </div>
     );
 }
